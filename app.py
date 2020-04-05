60556332039948048ab9ab763702ea25d8429cdf
simonvdv <simon@simonvan.be>
2020-04-05 23:29:45 +0200

    almost functionnal

--- app.py
+++ app.py
@@ -39,19 +39,15 @@ df2 = df2[['CASES', 'DEATHS', 'TOTAL_IN', 'TOTAL_IN_ICU', 'NEW_IN', 'NEW_OUT']]
 
 bar_cat = ['CASES', 'DEATHS', 'TOTAL_IN', 'TOTAL_IN_ICU']
 line_cat = ['NEW_IN', 'NEW_OUT']
-#categories = list(df2.columns)
-categories = bar_cat
+
+df_line = df2[line_cat]
 
 #easier to work with an un-tidy dataset in Bokeh
 def make_dataset(list_cat):
     #only chosen cat
-    
-    df_bar = df2[[i for i in list_cat if i in bar_cat]]
-    df_line = df2[line_cat]
-    
-    #df = df2[list_cat]
-    plot_df_bar = df_bar.reset_index().melt(['DATE']).set_index('DATE').sort_index() #adios tidy data :/
-    #plot_df_line = df_line.reset_index().melt(['DATE']).set_index('DATE').sort_index() #adios tidy data :/
+
+    plot_df_bar = df2[list_cat].reset_index().melt(['DATE']).set_index('DATE').sort_index() #adios tidy data :/
+
     #new format as typical melted DF/"multi-index without being multiindexed" :
     #DATE  variable value
     #day-1 CASES     28
@@ -59,42 +55,52 @@ def make_dataset(list_cat):
     #day-2 CASES (...)
     
     #random colors !FIX ME: nice colors
-    col_gen = lambda : "#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
-    colors = {i:col_gen() for i in bar_cat}
+    #deprecated :
+    #col_gen = lambda : "#"+''.join([random.choice('0123456789ABCDEF') for j in range(6)])
+    #colors = {i:col_gen() for i in bar_cat}
+    
+    colors = {'CASES':'grey', 'DEATHS':'darkgrey', 'TOTAL_IN':'orange', 'TOTAL_IN_ICU':'yellow'}
+    
     plot_df_bar["color"] = plot_df_bar["variable"].map(colors)
+    
+    #for better visibility : order by mean of variabl, in ascending order
+    
+    plot_df_bar['variable'] = pd.Categorical(plot_df_bar['variable'],\
+                        ['TOTAL_IN', 'CASES', 'TOTAL_IN_ICU','DEATHS'])
+    plot_df_bar = plot_df_bar.sort_values(['variable'])
+    return ColumnDataSource(plot_df_bar)
 
 
-
-    return ColumnDataSource(plot_df_bar), ColumnDataSource(df_line)
-
-
-def make_plot(src_bar, src_line):
+def make_plot(src_bar):
   
     
     # create a new plot with a datetime axis type
     p = figure(plot_width=700, plot_height=700, x_axis_type="datetime")
-    
-    
-    p.vbar(x='DATE', top='value', source = src_bar, fill_alpha = 0.7,\
+        
+    p.vbar(x='DATE', top='value', source = src_bar, fill_alpha = 1,\
            hover_fill_alpha = 1.0, line_color = 'black', width=dt.timedelta(1), \
-               color='color', legend_group='variable')
+               color='color', legend='variable', name="vbars")
+    
     
     
     colors = {'NEW_IN':'red', 'NEW_OUT':'green'}
-    p.line(x='DATE', y='NEW_IN', source=src_line, legend_label='NEW_IN', line_width=4, color=colors['NEW_IN'])
-    p.line(x='DATE', y='NEW_OUT', source=src_line, legend_label='NEW_OUT', line_width=4, color=colors['NEW_OUT'])
-
+    names = df_line.columns
+    for name in names:
+        p.line(x=df_line.index, y=df_line[name], legend_label=name, line_width=4, color=colors[name], name=name)
     
+
     #define tooltips    
     p.add_tools(HoverTool(tooltips = [('Date', '@DATE{%F}'), ('', '@variable: @value')],\
-                          formatters={'@DATE': 'datetime'}, point_policy="follow_mouse", mode="vline"))
+                          formatters={'@DATE': 'datetime'}, point_policy="follow_mouse", mode="vline", names=["vbars"]))
+        
+    p.add_tools(HoverTool(tooltips = [('', 'name: $y')],\
+                          point_policy="follow_mouse", mode="vline", names=list(names)))
 
     # attributes
-    p.title.text = "Count"
     p.legend.location = "top_left"
     p.grid.grid_line_alpha = 0
     p.xaxis.axis_label = 'Date'
-    p.yaxis.axis_label = 'Daily new cases'
+    p.yaxis.axis_label = 'Value'
     p.ygrid.band_fill_color = "olive"
     p.ygrid.band_fill_alpha = 0.1
 
@@ -103,10 +109,8 @@ def make_plot(src_bar, src_line):
 def update(attr, old, new):
     #chosen cat
     cat_to_plot = [cat_selection.labels[i] for i in cat_selection.active]
-    new_bar, new_line = make_dataset(cat_to_plot)
+    new_bar = make_dataset(cat_to_plot)
     src_bar.data.update(new_bar.data)
-    src_line.data.update(new_line.data)
-
 
 
 cat_selection = CheckboxGroup(labels=bar_cat, active=[0, 1])
@@ -116,7 +120,7 @@ controls = Column(cat_selection)
 init_cat = [cat_selection.labels[i] for i in cat_selection.active]
 
 
-src_bar, src_line = make_dataset(init_cat) 
-p = make_plot(src_bar, src_line)
+src_bar = make_dataset(init_cat) 
+p = make_plot(src_bar)
 layout = row(controls, p)
 curdoc().add_root(layout)
